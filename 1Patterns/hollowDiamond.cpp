fbda88182827da00032c0f5bc37b2f61b7af11ae
Saurabh Gokul Mhase <75540568+saurabh-mhase01@users.noreply.github.com>
2023-10-01 23:19:07 +0530

    Update hollowDiamond.cpp

--- 1Patterns/hollowDiamond.cpp
+++ 1Patterns/hollowDiamond.cpp
@@ -36,5 +36,33 @@ int main()
 			cout << "*\n";
 		}
 	}
+
+	
 	return 0;
+}
+//*
+//* *
+//* * *
+//* * * *
+//* * * * *
+//* * * *
+//* * *
+//* *
+//*
+#include<iostream>
+using namespace std;
+int main1(int argc, char *argv[])
+{
+	 for(int i = 0; i<5;i++){
+	 	for(int j=0;j<i;j++){
+	 		cout<<"* ";
+	 	}
+	 	cout<<endl;
+	 }
+	 for(int i = 0; i<5;i++){
+	 	for(int j=0;j<5-i;j++){
+	 		cout<<"* ";
+	 	}
+	 	cout<<endl;
+	 }
 }
