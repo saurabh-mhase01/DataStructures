5f7645e2d2bd20c736eef68cccbfcff9d07ea585
Saurabh Gokul Mhase <75540568+saurabh-mhase01@users.noreply.github.com>
2023-10-03 20:10:40 +0530

    Update patternn.cpp

--- 1Patterns/patternn.cpp
+++ 1Patterns/patternn.cpp
@@ -5,9 +5,27 @@ int main(){
     cout<<"enter size of pattern";
     cin>>num;
     for(int i =1;i<=num;i++){
-        for(int j=i;j<=num+i;j++){
+        for(int j=1;j<=num;j++){
+                if(j==i||j==num-i+1||i==num||i==1||j==1||j==num){
+                    cout<<"* ";
+                }else{
+                    cout<<"  ";
+                }
+        }
+        cout<<"\n";
+    }
+}
+
+
+
+int main2(){
+    int num;
+    cout<<"enter size of pattern";
+    cin>>num;
+    for(int i =1;i<=num;i++){
+        for(int j=i;j<=num+i-1;j++){
            
-                if(i==1||i==num||j==i||j==num+i){
+                if(i==1||i==num||j==i||j==num+i-1){
                     cout<<"* ";
                 }
                 else{
