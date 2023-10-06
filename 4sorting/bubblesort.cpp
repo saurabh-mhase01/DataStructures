2cb8b95c5ff17793a8bf5909f006ee1cf9fc7869
Saurabh Mhase <saurabsaiimhase@gmail.com>
2023-10-06 18:00:18 +0530

    sorting

---
+++ 4sorting/bubblesort.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+void swap(int* arr,int i,int j){
+    int t=arr[i];
+    arr[i]=arr[j];
+    arr[j]=arr[i];
+}
+int* bubbleSort(int* arr,int size){
+    for(int i =0;i<size;i++){
+        for(int j=i;j<size-1;j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr,j,j+1);
+            }  
+        }
+    }
+}
+void printarray(int* arr,int size){
+    for(int i =0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+}
+
+
+
+int main(){
+    int size ;
+    cout<<"enter size of array: ";
+    cin>>size;
+
+    int arr[size];
+    for(int i =0;i<size;i++){
+        cout<<"enter element :"<<i+1;
+        cin>>arr[i];
+    }
+    bubbleSort(arr,size);
+    printarray(arr,size);
+    
+}
+
---
+++ 4sorting/insertionsort.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+void swap(int* arr,int i,int j){
+    int t=arr[i];
+    arr[i]=arr[j];
+    arr[j]=arr[i];
+}
+int* insertionSort(int* arr,int size){
+    for(int i =0;i<size;i++){
+        int min =0 ,index=i;
+        for(int j=i+1;j<size;j++){
+            if(min>arr[j]){
+                min=arr[j];
+                index = i;
+            }  
+        }
+        swap(arr,i,index);
+    }
+}
+void printarray(int* arr,int size){
+    for(int i =0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+}
+
+int main(){
+    int size ;
+    cout<<"enter size of array: ";
+    cin>>size;
+
+    int arr[size];
+    for(int i =0;i<size;i++){
+        cout<<"enter element :"<<i+1;
+        cin>>arr[i];
+    }
+    insertionSort(arr,size);
+    printarray(arr,size);
+    
+}
+
