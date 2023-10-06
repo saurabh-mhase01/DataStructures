feb515a8cebb040daf54ff455f2131e218f98d15
Saurabh Mhase <saurabsaiimhase@gmail.com>
2023-10-06 19:39:37 +0530

    orting algorithms implemented

--- 4sorting/insertionsort.cpp
+++ 4sorting/insertionsort.cpp
@@ -7,11 +7,16 @@ void swap(int* arr,int i,int j){
     arr[j]=arr[i];
 }
 int* insertionSort(int* arr,int size){
-    for(int i =0;i<size;i++){
-        for(int j=i+1;j<size;j++){
-            
-        }
-        
+    int j=0;
+    for(int i =1;i<size;i++){
+           int  number = arr[i];
+           j=i-1;
+            while(j>=0&&arr[j] >number)
+            {
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=number;
     }
 }
 void printarray(int* arr,int size){
