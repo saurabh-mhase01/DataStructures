efa333892f7814dc88040906fe1f79274c0c2f1a
Saurabh Gokul Mhase <75540568+saurabh-mhase01@users.noreply.github.com>
2024-05-06 08:44:16 +0530

    Update selectionsort.cpp

--- 4sorting/selectionsort.cpp
+++ 4sorting/selectionsort.cpp
@@ -6,7 +6,7 @@ void swap(int* arr,int i,int j){
     arr[i]=arr[j];
     arr[j]=arr[i];
 }
-int* selectionSort(int* arr,int size){
+void selectionSort(int* arr,int size){
     for(int i =0;i<size;i++){
         int min =0 ,index=i;
         for(int j=i+1;j<size;j++){
